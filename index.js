067ec03433e451466cc8e3e9612adfe3a290dc21
timDeHof <this.is.tim.dehof@gmail.com>
2022-07-23 22:46:46 -0400

    added getnewdag function

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 // Remember to import the data and Dog class!
-import dogsData from './data.js'
-import Dog from './Dog.js'
+import dogsData from './data.js';
+import Dog from './Dog.js';
 
-let currentDogIndex = 0
-let currentDog = new Dog(dogsData[currentDogIndex])
-console.log(currentDog.getDogHtml())
-document.getElementById('accept-button').addEventListener('click', yes)
+let currentDogIndex = 0;
+let currentDog = new Dog(dogsData[currentDogIndex]);
+console.log(currentDog.getDogHtml());
+document.getElementById('accept-button').addEventListener('click', yes);
 
-render()
+render();
 
 function render() {
-    document.getElementById('card').innerHTML = currentDog.getDogHtml()
+  document.getElementById('card').innerHTML = currentDog.getDogHtml();
+}
+
+// get the next dogs data and render it
+function getNewDog() {
+  currentDogIndex++;
+  currentDog = new Dog(dogsData[currentDogIndex]);
+  render();
 }
 
 function yes() {
-    console.log("clicked")
-}co
+  currentDog.setMatchStatus(true);
+  getNewDog();
+}
